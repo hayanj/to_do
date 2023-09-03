76269bb1b3a4deef00224875c408c61280c0ee98
hayanj <hayaaljuraysi@gmail.com>
2023-09-03 21:59:09 +0200

    added completed column

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ class ToDo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     def __repr__(self):
         return f'<ID: {self.id}, Description: {self.description}>'
 
