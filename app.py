9e9b3130cf51622ab6989d50f7bc95cb2ee8620e
hayanj <hayaaljuraysi@gmail.com>
2023-09-03 21:25:33 +0200

    initial commit, database created with main functionalities

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import config
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.database_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    def __repr__(self):
+        return f'<ID: {self.id}, Description: {self.description}>'
+
+@app.route('/')
+def index():
+    return 'Hello'
